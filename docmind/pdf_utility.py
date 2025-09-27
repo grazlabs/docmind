7b2929e52b0b5bf807d92abc8632dba25bcf5d6e
graz <parrotabhijeet@gmail.com>
2025-09-28 01:42:59 +0530

    Function syntax added in the pdf_utility file.

---
+++ docmind/pdf_utility.py
@@ -0,0 +1,8 @@
+def text_from_pdf(pdf_file)->str:
+    '''
+        Takes a pdf file as input.
+        Returns a string containing
+        the text content extracted from the pdf.
+    '''
+
+    return "Not implemented."

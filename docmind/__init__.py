d3db55e794aea0947859dd3677d8c4c7409f853b
graz <parrotabhijeet@gmail.com>
2025-09-28 01:37:41 +0530

    Initial templates setup added. launch_dev command refined.

--- docmind/__init__.py
+++ docmind/__init__.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template
 from llm import query_processor
-import pdf_utility
+from docmind import pdf_utility
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route("/", methods=('GET',))
-    def index():
-        return "Hello", 200
+    def index_page():
+        return render_template("index_page.html"), 200
     
     @app.errorhandler(404)
     def page_not_found(e):
-        return "OOPS! Page not found.", 404
+        return render_template("page_not_found.html"), 404
     
     @app.route("/query_page", methods=('GET',))
     def query_page():
-        return "Prompt Page", 200
+        return render_template("query_page.html"), 200
     
     @app.route("/response_page", methods=('POST',))
-    def result_page():
-        return 
+    def response_page():
+        return render_template("response_page.html"), 200
     
     return app

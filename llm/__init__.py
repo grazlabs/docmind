d1d06a6eb877ff7537583a32989ea140461b8d4a
graz <parrotabhijeet@gmail.com>
2025-09-29 01:52:49 +0530

    file handling added in response_page route with processing for plain and markdown files complete and remaining for pdf. Syntax of query_processor function updated and docstring added.

--- llm/__init__.py
+++ llm/__init__.py
@@ -1,2 +1,9 @@
-def query_processor(context_data:list[str], prompt:str)->str:
-    return "Output after processing the query."
+def query_processor(context_data:dict[str,str], prompt:str)->str:
+    '''
+        - context_data contains the text of files attached by user.
+        - It is a dictionary where filename is the key
+          and respective value is the file's text content.
+        - The prompt is a string containing the query provided by user.
+        - The return value is a string (containing the answer to prompt).
+    '''
+    return "Not implemented yet. context_data = " + context_data.__str__()
